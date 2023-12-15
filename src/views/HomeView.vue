a4ad831adffb688f5612c85061ab65effaeebc6b
MasahiKon <konnonorth@gmail.com>
2023-12-14 19:27:25 -0800

    remove defaults

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,47 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+
+const isPressed = ref(false)
+const detectA = () => {
+  isPressed.value = !isPressed.value
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div tabindex="0" @keydown.a="detectA" :class="{ pressed: isPressed }">a</div>
+    <button>Start</button>
   </main>
 </template>
+
+<style>
+/*@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}*/
+
+main {
+padding: 0;
+margin: 0;
+}
+
+div {
+  width: 50vw;
+  height: 50vh;
+  background-color: aqua;
+  margin: 0px;
+  padding: 0px;
+  border: black 2px solid;
+}
+
+div:focus {
+  outline: none;
+}
+
+.pressed {
+  color: red;
+}
+</style>

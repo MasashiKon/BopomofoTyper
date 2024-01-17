63cf6a5571288626bcd1c5dde9acfff4334b6a83
MasahiKon <konnonorth@gmail.com>
2024-01-16 16:40:11 -0800

    rmv db filter

--- src/utils/generateQuery.ts
+++ src/utils/generateQuery.ts
@@ -3,7 +3,7 @@ import { Level } from '@/type/enums'
 export default (level: Level, innerLevel: number): string => {
   //(filter: {id: {eq: }})
   return `{
-        sentences_${level}_${innerLevel}Collection(filter: {id: {eq: 20}}) {
+        sentences_${level}_${innerLevel}Collection {
           edges {
             node {
               id
